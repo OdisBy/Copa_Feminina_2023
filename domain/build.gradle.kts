5496975d915f2eca00d6f27e1033033365e91a56
Ruliam dos Santos de Oliveira <ruliam.oliveira@gmail.com>
2023-07-25 21:36:57 -0300

    Fix Domain module being android module

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -1,38 +1,11 @@
 plugins {
-    alias(libs.plugins.androidApplication)
+    alias(libs.plugins.androidLibrary)
     alias(libs.plugins.kotlinAndroid)
 }
 
 android {
-    namespace = "com.odisby.domain"
+    namespace = "com.odisby.copa_feminina.domain"
     compileSdk = rootProject.extra["compileSdkVersion"] as Int
-
-    defaultConfig {
-        applicationId = "com.odisby.domain"
-        minSdk = rootProject.extra["minSdkVersion"] as Int
-        targetSdk = rootProject.extra["targetSdkVersion"] as Int
-        versionCode = 1
-        versionName = "1.0"
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
 }
 
 dependencies {
